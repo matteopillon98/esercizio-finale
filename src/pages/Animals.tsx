e58d62cc4cfd7d2ac4fab3d9348f867d221af9b8
Matteo Pillon <matteopillon98@gmail.com>
2023-01-13 17:59:33 +0100

    Try to add custom data

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -3,8 +3,7 @@ import axios from "axios";
 import List from "@mui/material/List";
 import Animal from "./Animal";
 
-const baseURL =
-  "https://petstore.swagger.io/v2/pet/findByStatus?status=available";
+const baseURL = "https://petstore.swagger.io/v2/pet/findByStatus?status=sold";
 
 const Animals = () => {
   const [animals, setAnimals] = useState([]);
@@ -13,6 +12,27 @@ const Animals = () => {
     axios.get(baseURL).then((response) => {
       setAnimals(response.data);
     });
+
+    axios
+      .post("https://petstore.swagger.io/v2/pet", {
+        id: Math.floor(Math.random() * (animals.length + 10) + 1),
+        category: {
+          id: 0,
+          name: "string",
+        },
+        name: "pippo",
+        photoUrls: ["string"],
+        tags: [
+          {
+            id: 0,
+            name: "string",
+          },
+        ],
+        status: "sold",
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
   }, []);
 
   return (
@@ -23,13 +43,15 @@ const Animals = () => {
           animals
             .slice(animals.length - 10, animals.length)
             .map((animal, index) => {
-              return (
-                <Animal
-                  id={animal.id}
-                  key={animal.id + index}
-                  name={animal.name}
-                ></Animal>
-              );
+              if (animal.name) {
+                return (
+                  <Animal
+                    id={animal.id}
+                    key={animal.id + index}
+                    name={animal.name}
+                  ></Animal>
+                );
+              }
             })}
       </List>
     </div>
